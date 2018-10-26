e5f0672ebea490e4324d46dca8347d4cd4b2a15c
Hong, Jian-Ching <allyusd@users.noreply.github.com>
2018-10-26 23:32:34 +0800

    Update commonPipeline.groovy

--- vars/commonPipeline.groovy
+++ vars/commonPipeline.groovy
@@ -1,5 +1,32 @@
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+def call() {
+    pipeline {
+        agent none
+        stages {
+            stage('parallel') {
+                agent {
+                    label 'test'
+                }
+                steps {
+                    script {
+                        def tasks = [:]
+                        def imagelist = readFile('imagelist').replace("\n", "").split(',')
+                        for (image in imagelist) {
+                            def image_inside = "${image}"
+                            tasks["${image}"] = {
+                                node('docker') {
+                                    stage("${image_inside}") {
+                                        docker.image("${image_inside}").inside {
+                                            sh "echo ${image_inside}"
+                                            sh 'cat /etc/*-release'
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        parallel tasks
+                    }
+                }
+            }
+        }
+    }
 }
